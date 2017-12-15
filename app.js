f1c50dc443a5b6ea7f823926c14bd82feeea666d
Tess Craig <tessashleycraig89@gmail.com>
2017-12-15 00:00:40 -0600

    Commit 9 - moved some variable definintions into functions, fix pushToArray function so postBoxValue is emptied after

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
-//get and store postBox value
-//get and store posts li
-//get and store postButton
+//get and declare var for post button and posts
 
-// make empty madePosts array for <li> of madePosts
+//make madePosts array
 
-//postButton. addEventListener 'click'
-//'click' triggers function that pushes postBox value into front of madePosts array using unshift
-	//then sets postBox value to null
-	//then call publishPost
+//add click event and push to array function to postButton
+	//get and declar var value of postBox
+		//unshift(move to front of array) postBox value into madePosts
+		//set postBoxValue to zero
+		//call publishPost function
 
 //publishPost function
-	//make new list element and store as liElement
-	//take madePosts[i] and store in content variable
-	//liElement append content
-	//unshift liElement to posts
-
-let postBoxValue = document.getElementById('postBox').value;
-
+	//create list element
+	//let content start with 0 post
+		//appendchild liElement to posts array to add to the page
 const postButton = document.getElementById('postButton')
 
 let posts = document.getElementById('posts')
@@ -25,21 +20,15 @@ let madePosts = [];
 
 
 postButton.addEventListener('click', pushToArray = () => {
-	madePosts.unshift(postBoxValue);
-	postBoxValue = null;
-	publishPost();
+  let postBoxValue = document.getElementById('postBox').value;
+    madePosts.unshift(postBoxValue);
+    postBoxValue = null;
+    publishPost();
 })
 
-
 const publishPost = () => {
-	let liElement= document.createElement('LI');
-	let content = madePosts[0]
-	liElement.append(content);
-	posts.appendChild(liElement);
+    let liElement= document.createElement('LI');
+    let content = madePosts[0];
+    liElement.append(content);
+    posts.appendChild(liElement);
 }
-	// let liElement = document.createElement('LI');
-	// let content = madePosts[0]
-	// liElement.append(content);
-	// posts.appendChild(liElement);
-
-//I don't know why undefined is the first list item and I want to unshift the liElement into posts but it won't work so I'm resorting to appendChild

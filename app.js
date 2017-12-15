61d520eb1133a3aa64ca033fa29e92c6bd31607f
Tess Craig <tessashleycraig89@gmail.com>
2017-12-14 23:26:00 -0600

    Commit 8 - undefined no longer shows

--- app.js
+++ app.js
@@ -37,9 +37,9 @@ const publishPost = () => {
 	liElement.append(content);
 	posts.appendChild(liElement);
 }
-	let liElement = document.createElement('LI');
-	let content = madePosts[0]
-	liElement.append(content);
-	posts.appendChild(liElement);
+	// let liElement = document.createElement('LI');
+	// let content = madePosts[0]
+	// liElement.append(content);
+	// posts.appendChild(liElement);
 
 //I don't know why undefined is the first list item and I want to unshift the liElement into posts but it won't work so I'm resorting to appendChild
